f730a20bc0c4d95ecb455a6dc0f062d0b979cc74
German G. Bertea <german.bertea@santexgroup.com>
2018-10-02 11:46:24 -0300

    tarea 1 seccion 2

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,9 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  // selector: '[app-servers]',
   selector: 'app-servers',
   templateUrl: './servers.component.html',
+  // template:'<app-server></app-server><app-server></app-server>',
   styleUrls: ['./servers.component.css']
+    //   styles:[`h3{
+    //     color: dodgerblue;
+    // }`]
 })
 export class ServersComponent implements OnInit {
 
